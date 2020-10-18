706b127add220ccf41e9ef92e08c080f89e9d62f
Sushmitha Shetty <72378041+sushmithashetty044@users.noreply.github.com>
2020-10-18 22:37:16 +0530

    Add files via upload

---
+++ module 4 solution/SpeakHello.js
@@ -0,0 +1,10 @@
+8 lines (8 sloc)  232 Bytes
+  
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
